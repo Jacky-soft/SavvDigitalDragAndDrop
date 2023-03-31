3be4a62820f1a74125ccf21579091dd189f8c207
Jacky <jacky.li@outlook.co.nz>
2023-04-01 11:02:26 +1300

    drag and drop is working.

--- app/src/main/java/studio/apping/savvdigitaldraganddrop/MainActivity.kt
+++ app/src/main/java/studio/apping/savvdigitaldraganddrop/MainActivity.kt
@@ -42,70 +42,38 @@ fun MainScreen() {
     LongPressDraggable(modifier = Modifier.fillMaxSize()) {
         val itemMatrix: MutableState<List<List<String>>> = remember { mutableStateOf(
             listOf(
-                listOf("a"),
-                listOf("b"),
-                listOf("c"),
-                listOf("d"),
-                listOf("e"),
-                listOf("f"),
-            )) } //TODO fix the add and remove logic. Add drop text shows nothing but index. Drag text shows the actual content.
-        val addRow: (Int, String) -> Unit = { atIndex: Int, newElement: String ->
-            val matrix = mutableListOf<List<String>>()
-            itemMatrix.value.forEach { matrix.add(it) }
-            matrix.add(atIndex, listOf(newElement))
-            itemMatrix.value = matrix
-        }
+                listOf("a","b"),
+                listOf("c","d"),
+                listOf("e","f"),
+            )) }
 
-        val addColumn: (Int, Int, String) -> Unit = { atIndex: Int, onLeft: Int, newElement: String ->
-            val existingElement = itemMatrix.value[atIndex].first()
-            val updatedRow = if (onLeft == 0) listOf(newElement, existingElement) else listOf(existingElement, newElement)
+        //TODO make the drag layout and drop layout less gap between the rows.
+        DropLayout(itemMatrix) { from, to ->
             val matrix = mutableListOf<List<String>>()
-            itemMatrix.value.forEach {
-                matrix.add(it)
+            matrix.addAll(itemMatrix.value)
+            val element = matrix.getElement(from.first, from.second)
+
+            if (from.third) {
+                matrix.removeRow(from.first)
+            } else {
+                matrix.removeColumn(from.first, from.second)
             }
-            matrix[atIndex] = updatedRow
-            itemMatrix.value = matrix
-        }
 
-        val removeRow: (Int) -> Unit = { atIndex: Int ->
-            val matrix = mutableListOf<List<String>>()
-            matrix.addAll(itemMatrix.value)
-            matrix.removeAt(atIndex)
-            itemMatrix.value = matrix
-        }
+            if (to.third) {
+                matrix.addRow(to.first, element)
+            } else {
+                matrix.addColumn(to.first, to.second, element)
+            }
 
-        val removeColumn: (Int, Int) -> Unit = { atRowIndex: Int, atColumnIndex: Int ->
-            val matrix = mutableListOf<List<String>>()
-            matrix.addAll(itemMatrix.value)
-            val targetRow = mutableListOf<String>()
-            targetRow.addAll(matrix[atRowIndex])
-            targetRow.removeAt(atColumnIndex)
-            matrix[atRowIndex] = targetRow
             itemMatrix.value = matrix
         }
-//TODO make the drag layout and drop layout less gap between the rows.
-        DragLayout(itemMatrix,
-            { index ->
-                removeRow(index)
-            },
-            { atRowIndex, atColumnIndex ->
-                removeColumn(atRowIndex, atColumnIndex)
-        })
-        DropLayout(itemMatrix,
-            { index, element ->
-                addRow(index, element)
-            },
-            { atRowIndex, atColumnIndex, element ->
-                addColumn(atRowIndex, atColumnIndex, element)
-        })
+        DragLayout(itemMatrix)
     }
 }
 
 @Composable
 fun DragLayout(
-    dragMatrix: MutableState<List<List<String>>>,
-    removeRowAt: (Int) -> Unit,
-    removeColumnAt: (Int, Int) -> Unit
+    dragMatrix: MutableState<List<List<String>>>
 ) {
     Column(
         Modifier
@@ -116,23 +84,11 @@ fun DragLayout(
         for (i in 0..dragMatrix.value.lastIndex) {
             val currentDragRow = dragMatrix.value[i]
             if (currentDragRow.size == 1) {
-                DragBox(Modifier.height(60.dp), currentDragRow.first()) {
-                    removeRowAt(i)
-                }
+                DragBox(Modifier.height(60.dp), Triple(i, 0, true))
             } else if (currentDragRow.size == 2) {
                 Row {
-                   DragBox(
-                       Modifier
-                           .weight(1f)
-                           .height(60.dp), currentDragRow.first()) {
-                       removeColumnAt(i, 0)
-                   }
-                   DragBox(
-                       Modifier
-                           .weight(1f)
-                           .height(60.dp), currentDragRow.last()) {
-                       removeColumnAt(i, 1)
-                   }
+                   DragBox(Modifier.weight(1f).height(60.dp), Triple(i,0,false))
+                   DragBox(Modifier.weight(1f).height(60.dp), Triple(i,1,false))
                 }
             }
             Spacer(modifier = Modifier.height(30.dp))
@@ -143,21 +99,15 @@ fun DragLayout(
 @Composable
 fun DropLayout(
     dropMatrix: MutableState<List<List<String>>>,
-    createNewRowAt: (Int, String) -> Unit,
-    createNewColumnAt: (Int, Int, String) -> Unit
-) {  // TODO if the drag is size 1, then can not drop at the same row.
+    updateMatrixFunc: (Triple<Int, Int, Boolean>, Triple<Int, Int, Boolean>) -> Unit
+) {
 
     Column(
-        Modifier
-            .fillMaxSize()
-            .padding(2.dp)) {
-
+        Modifier.fillMaxSize().padding(2.dp)) {
         var matrixIndex = 0
         DropBox(modifier = Modifier
             .fillMaxWidth()
-            .height(30.dp), temMatrixIndex = matrixIndex) { newSingleElement ->
-            createNewRowAt(matrixIndex / 2, newSingleElement)
-        }
+            .height(30.dp),  Triple(matrixIndex/2, 0, true)) { from, to -> updateMatrixFunc(from, to)}
         for (i in 1..dropMatrix.value.size) {
             matrixIndex += 1
             val currentRow = dropMatrix.value[matrixIndex / 2]
@@ -166,17 +116,11 @@ fun DropLayout(
                 Row {
                     DropBox(modifier = Modifier
                         .height(60.dp)
-                        .weight(1f), temMatrixIndex = matrixIndex) { newColumnValue ->
-                        val updatedAtIndex = matrixIndex / 2
-                        createNewColumnAt(updatedAtIndex, 0, newColumnValue)
-                    }
+                        .weight(1f), Triple(matrixIndex/2, 0, false)) { from, to -> updateMatrixFunc(from, to)}
                     Spacer(modifier = Modifier.width(4.dp))
                     DropBox(modifier = Modifier
                         .height(60.dp)
-                        .weight(1f), temMatrixIndex = matrixIndex) { newColumnValue ->
-                        val updatedAtIndex = matrixIndex / 2
-                        createNewColumnAt(updatedAtIndex, 1, newColumnValue)
-                    }
+                        .weight(1f), Triple(matrixIndex/2, 1, false)) { from, to -> updateMatrixFunc(from, to)}
                 }
             } else {
                 ItemArea(modifier = Modifier.height(60.dp), note = "Full $currentRow")
@@ -184,62 +128,58 @@ fun DropLayout(
             matrixIndex += 1
             DropBox(modifier = Modifier
                 .fillMaxWidth()
-                .height(30.dp), temMatrixIndex = matrixIndex) { newSingleElement ->
-                val elementIndex = matrixIndex / 2
-                createNewRowAt(elementIndex, newSingleElement)
+                .height(30.dp), Triple(matrixIndex/2, 0, true)) { from, to -> updateMatrixFunc(from, to)
             }
         }
     }
 }
 
 @Composable
-fun DragBox(modifier: Modifier, value: String, removeRow: () -> Unit) {
+fun DragBox(modifier: Modifier, from: Triple<Int, Int, Boolean>) {
     DragTarget(
-        modifier = modifier, dataToDrop = value,
-        removeRow
+        modifier = modifier, dataToDrop = from,
     ) {
-        ItemArea(modifier = modifier, note = value)
+        ItemArea(modifier = modifier, note = from.toString())
     }
 }
 
 @Composable
-fun DropBox(modifier: Modifier, temMatrixIndex: Int, newSingleElement: (String) -> Unit) { //TODO temMatrixIndex can be removed when refactor
-    Box(modifier) {
-        DropTarget<String>(
-            modifier = modifier
-        ) { isInBound, data ->
-            val bgColor = if (isInBound) {
-                Color.Red
-            } else {
+fun DropBox(modifier: Modifier, to: Triple<Int, Int, Boolean>, newSingleElement: (Triple<Int, Int, Boolean>, Triple<Int, Int, Boolean>) -> Unit) { //TODO temMatrixIndex can be removed when refactor
+    DropTarget<Triple<Int,Int,Boolean>>(
+        modifier = modifier
+    ) { isInBound, data ->
+        val bgColor = if (isInBound) {
+            Color.Red
+        } else {
 //                Color.White // Be transparent for dev purpose
-                Color.Transparent // Transparent overlap looks confusing
-            }
+            Color.Transparent // Transparent overlap looks confusing
+        }
 
-            data?.let {
-                if (isInBound) {
-                    newSingleElement(it)
-                }
+        data?.let {
+            if (isInBound) {
+                val from = it
+                newSingleElement(from, to)
             }
+        }
 
-            Column(
-                modifier = modifier
-                    .fillMaxWidth()
-                    .shadow(elevation = 4.dp, shape = RoundedCornerShape(16.dp))
-                    .background(
-                        //                Color.White // Be transparent for dev purpose
-                        bgColor,
-                        RoundedCornerShape(16.dp)
-                    ),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text(
-                    text =  "Drop",
-                    fontSize = 18.sp,
-                    color = Color.Black,
-                    fontWeight = FontWeight.Bold
-                )
-            }
+        Column(
+            modifier = modifier
+                .fillMaxWidth()
+                .shadow(elevation = 4.dp, shape = RoundedCornerShape(16.dp))
+                .background(
+                    //                Color.White // Be transparent for dev purpose
+                    bgColor,
+                    RoundedCornerShape(16.dp)
+                ),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text =  "Drop",
+                fontSize = 18.sp,
+                color = Color.Black,
+                fontWeight = FontWeight.Bold
+            )
         }
     }
 }
@@ -251,8 +191,8 @@ fun ItemArea(modifier: Modifier, note: String) {
             .fillMaxWidth()
             .shadow(elevation = 4.dp, shape = RoundedCornerShape(16.dp))
             .background(
-                //                Color.White // Be transparent for dev purpose
-                Color.Transparent,
+                                Color.White, // Be transparent for dev purpose
+//                Color.Transparent,
                 RoundedCornerShape(16.dp)
             ),
         verticalArrangement = Arrangement.Center,
--- app/src/main/java/studio/apping/savvdigitaldraganddrop/MutableListExtension.kt
+++ app/src/main/java/studio/apping/savvdigitaldraganddrop/MutableListExtension.kt
@@ -1,2 +1,29 @@
 package studio.apping.savvdigitaldraganddrop
 
+fun <T> MutableList<List<T>>.getElement(fromRow: Int, fromColumn: Int) : T{
+    val row = this[fromRow]
+    return row[fromColumn]
+}
+
+fun <T> MutableList<List<T>>.removeRow(fromRow: Int) {
+    this.removeAt(fromRow)
+}
+
+fun <T> MutableList<List<T>>.removeColumn(fromRow: Int, fromColumn: Int) {
+    val row = mutableListOf<T>()
+    row.addAll(this[fromRow])
+    row.removeAt(fromColumn)
+    this[fromRow] = row
+}
+
+fun <T> MutableList<List<T>>.addRow(toRow: Int, element: T) {
+    val newRow = listOf(element)
+    this.add(toRow, newRow)
+}
+
+fun <T> MutableList<List<T>>.addColumn(toRow: Int, toColumn: Int, element: T) {
+    val row = mutableListOf<T>()
+    row.addAll(this[toRow])
+    row.add(toColumn, element)
+    this[toRow] = row
+}
